4d10c824070bc9a9fe22e302c44ab8ca83db17ef
hsma-chief-elf <d.chalk@exeter.ac.uk>
2024-09-25 13:34:54 +0100

    PIMS.PY: added initial questions

--- pims.py
+++ pims.py
@@ -2,3 +2,16 @@ import streamlit as st
 
 st.title("Welcome to PIMS - The PenCHORD Impact Store")
 
+new_name = st.text_input(
+    "What's your name?"
+)
+
+new_area = st.selectbox(
+    "Which area?",
+    ["HSMA", "Core PenCHORD"]
+)
+
+new_burst = st.text_area(
+    "What do you want to tell us? (Max 200 characters)",
+    max_chars=200)
+
