8c9822688134e6b390288776301bf1b6260e01e8
hsma-chief-elf <d.chalk@exeter.ac.uk>
2024-09-25 15:25:18 +0100

    PIMS.PY: added month and year input

--- pims.py
+++ pims.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+from datetime import datetime
 
 st.title("Welcome to PIMS - The PenCHORD Impact Store")
 
@@ -13,6 +15,24 @@ with col_left:
         "Which area of work?",
         ["HSMA", "Core PenCHORD"]
     )
+
+    col_month, col_year = st.columns(2)
+
+    with col_month:
+        new_month = st.selectbox(
+            "Month",
+            ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug",
+             "Sep", "Oct", "Nov", "Dec"],
+             index = (datetime.today().month - 1)
+        )
+
+    with col_year:
+        new_year = st.number_input(
+            "Year",
+            min_value=2010,
+            max_value=2099,
+            value=(datetime.today().year)
+        )
 
 with col_right:
     new_burst = st.text_area(
