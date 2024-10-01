346831b5d8bad524ea6a96eee171983178550213
hsma-chief-elf <d.chalk@exeter.ac.uk>
2024-10-01 11:41:57 +0100

    PIMS.PY: working test of supabase db read

--- pims.py
+++ pims.py
@@ -1,6 +1,11 @@
 import streamlit as st
-from streamlit_gsheets import GSheetsConnection
 from datetime import datetime
+#from st_supabase_connection import SupabaseConnection
+from supabase import create_client, Client
+
+# https://docs.streamlit.io/develop/tutorials/databases/supabase
+# Need to copy app secrets to the cloud on deployment - see instructions in link
+# above
 
 st.title("Welcome to PIMS - The PenCHORD Impact Store")
 
@@ -38,8 +43,35 @@ with col_right:
     new_burst = st.text_area(
         "What do you want to tell us? (Max 200 characters)",
         max_chars=200)
-
-new_link = st.text_input(
+    
+    new_link = st.text_input(
     "Is there a link where we can find out more?"
-)
+    )
+
+# Initialise Supabase connection
+#conn = st.connection("supabase", type=SupabaseConnection)
+
+# Perform query
+#rows = conn.query("*", table="pims_table", ttl="10m").execute()
+
+# Print results
+#for row in rows.data:
+#    st.write(f"{row['name']} said {row['blurb']}")
+
+@st.cache_resource
+def init_connection():
+    url = st.secrets["SUPABASE_URL"]
+    key = st.secrets["SUPABASE_KEY"]
+    return create_client(url, key)
+
+supabase = init_connection()
+
+@st.cache_resource()
+def run_query():
+    return supabase.table("pims_table").select("*").execute()
+
+rows = run_query()
+
+for row in rows.data:
+    st.write(f"{row['name']} said {row['blurb']}")
 
