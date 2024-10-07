69a87ce8c2892d4a004b5f41f0f3e0be45258723
hsma-chief-elf <d.chalk@exeter.ac.uk>
2024-10-07 12:50:53 +0100

    PIMS.PY: corrected additional stopword functionality to use update()

--- pims.py
+++ pims.py
@@ -15,7 +15,8 @@ st.set_page_config(layout="wide",
 
 # Create set of stopwords and punctuation mapping table for wordcloud generation
 stopwords = set(STOPWORDS)
-stopwords.add("ive")
+additional_exclusion_words = ["ive"]
+stopwords.update(additional_exclusion_words)
 punctuation_mapping_table = str.maketrans('', '', string.punctuation)
 
 # Load SpaCy model
